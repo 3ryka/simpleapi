86ac06237eda99cca4b1f84779a78758eb341deb
3ryka <saltedpopcorn48@gmail.com>
2024-11-12 19:33:34 +0700

    add comments and change return value

--- api/main.py
+++ api/main.py
@@ -4,7 +4,7 @@ app = FastAPI() #create an instance of FastAPI class and assign to variable app
 
 @app.get("/") #define a route (URL path that clients can use to access API)
 async def hello(): # an asynchronous (async) function; using async in fastapi makes code faster when handling many requests at the same time
-    print("Hello, this is the simple API deployment task that I did!")
+    return "Hello, this is the simple API deployment task that I did!"
 
 # more about @app.get("/") 
 # @app.get means when someone visits a certain web address or path, the API is gonna do something
